6624ee8cd31eee8d78a4d05b532b405babfeaeb1
Jeremy Liu <jeremyliu@Jeremys-MacBook-Pro-16.local>
2025-01-01 15:02:18 -0700

    Redesign support page

--- static/helpers/helpers.js
+++ static/helpers/helpers.js
@@ -4,3 +4,27 @@ function pluralize(word, amount) {
     }
     return word + "s";
 }
+
+
+function setAccordions() {
+    var accButton = document.getElementsByClassName("accordion-button");
+    var i;
+
+    for (i = 0; i < accButton.length; i++) {
+        accButton[i].addEventListener("click", event => {
+            var element = event.target;
+            element.classList.toggle("accordion-active");
+
+            var accPanel = element.nextElementSibling;
+            if (accPanel.style.maxHeight) {
+                accPanel.style.transition = null;
+                accPanel.style.maxHeight = null;
+                element.style.borderRadius = "10px";
+            } else {
+                accPanel.style.transition = "max-height 0.2s ease-out";
+                accPanel.style.maxHeight = accPanel.scrollHeight + "px";
+                element.style.borderRadius = "10px 10px 0 0";
+            }
+        });
+    }
+}
