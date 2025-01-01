078a541943e88981bbb43c759e6374ed9ec279d0
Jeremy Liu <jeremyliu@Jeremys-MacBook-Pro-16.local>
2024-12-31 20:39:27 -0700

    Add homepage animations

--- static/helpers/constants.js
+++ static/helpers/constants.js
@@ -513,3 +513,18 @@ const allCommands = [
     { name: "logoff", description: "Logs the bot off from Discord's Gateway. Creator use only.", aliases: ["log-off", "logout", "log-out"], usage: "logoff", example: "logoff", cooldown: "2 secs", premium: false, category: "creator", disable: false, nsfw: false },
     { name: "restart", description: "Reconnects the bot to Discord's Gateway. Creator use only.", aliases: ["reboot"], usage: "restart", example: "restart", cooldown: "2 secs", premium: false, category: "creator", disable: false, nsfw: false },
 ];
+
+
+const categoryConstants = [
+    { category: "configuration", emoji: "⚙️", color: "#4c3791", textColor: "#bebebe" },
+    { category: "economy", emoji: "💰", color: "#bdad44", textColor: "#4e4e4e" },
+    { category: "fun", emoji: "😄", color: "#96457f", textColor: "#bebebe" },
+    { category: "games", emoji: "🎮", color: "#913a3a", textColor: "#bebebe" },
+    { category: "images", emoji: "📷", color: "#753f9e", textColor: "#bebebe" },
+    { category: "moderation", emoji: "🛡️", color: "#8f5e2e", textColor: "#bebebe" },
+    { category: "music", emoji: "🎵", color: "#2d8767", textColor: "#bebebe" },
+    { category: "sciences", emoji: "🧪", color: "#37782f", textColor: "#bebebe" },
+    { category: "statistics", emoji: "📊", color: "#3a8791", textColor: "#bebebe" },
+    { category: "text", emoji: "🆗", color: "#879126", textColor: "#4e4e4e" },
+    { category: "utility", emoji: "🛠️", color: "#3a6391", textColor: "#bebebe" },
+];
